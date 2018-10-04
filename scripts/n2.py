5caaf5cf67101c4c35b737ac4b5e164bfc57316e
Unknown <ponur0kirill@gmail.com>
2018-10-05 00:43:28 +0300

    гыг

--- scripts/n2.py
+++ scripts/n2.py
@@ -7,15 +7,15 @@ l,I=parsing(name,0,1)
 #из графика для ртутной лампы по 4 точкам соотнесли градусы на шкале с длинами волн
 
 def f(x): #x-- градусы
-    p1 =-3.672*10**(-7)
-    p2=-9.204*10**(-5)
-    p3 =7.457
+    p1 =-3.668*10**(7)
+    p2=-9.329*10**(9)
+    p3 =7.457*10**(14)
     f = p1*x**2 + p2*x + p3
     return f
 x=np.arange(200,3500,1)
 plt.plot(x,f(x))
 plt.grid () 
-plt.ylabel('$\\nu$,$10^{14}$ Гц',fontsize=12) 
+plt.ylabel('$\\nu$, Гц',fontsize=12) 
 plt.xlabel('градусы на монохроматоре',fontsize=12)# 
 plt.savefig('podgon.pdf')
 plt.show()
@@ -26,7 +26,7 @@ plt.figure('Задание 5.2')
 plt.plot(freq,I,'b')
 plt.plot(freq,I,'ro')
 plt.ylabel('$I_ф$,мА',fontsize=12) 
-plt.xlabel('$\\nu,10^{14}$Гц',fontsize=12)# 
+plt.xlabel('$\\nu,Гц',fontsize=12)# 
 plt.yticks([i for i in range(0,50,4)]) 
 plt.grid () 
 # plt.savefig('n2.pdf')
